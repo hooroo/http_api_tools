ca354ff8b5c7ed79cfe416c0cd8a54ea804d860f
Rob Monie <robmonie@gmail.com>
2013-11-11 11:39:47 +1100

    include main files in hat.rb

--- lib/hat.rb
+++ lib/hat.rb
@@ -1,4 +1,8 @@
 require "hat/version"
+require 'hat/json_serializer'
+require 'hat/json_deserializer'
+require 'hat/json_model'
+require 'hat/relation_includes'
 
 module Hat
   # Your code goes here...
