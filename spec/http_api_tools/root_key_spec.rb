326a9a85e68779e55d805b11bf1f3286703cb568
Jacob Evans <jacob@dekz.net>
2016-04-05 12:02:29 +1000

    Rename class files

--- spec/http_api_tools/root_key_spec.rb
+++ spec/http_api_tools/root_key_spec.rb
@@ -4,13 +4,13 @@ require 'http_api_tools/json_serializer_dsl'
 module HttpApiTools
 
   describe 'RootKey' do
-    class TestSerializer
+    class RootKeyTestSerializer
       include HttpApiTools::Nesting::JsonSerializer
 
       serializes(String, group: :foo)
     end
 
-    class TestTwoSerializer
+    class RootKeyTestTwoSerializer
       include HttpApiTools::Nesting::JsonSerializer
 
       serializes(Hash, group: :foo)
@@ -18,18 +18,18 @@ module HttpApiTools
 
     let(:serialized) { serializer.new(serializable).as_json }
 
-    context TestSerializer do
+    context RootKeyTestSerializer do
       let(:serializable) { 'the string' }
-      let(:serializer)   { TestSerializer }
+      let(:serializer)   { RootKeyTestSerializer }
 
       it 'has the correct root key' do
         expect(serialized).to include(:strings)
       end
     end
 
-    context TestTwoSerializer do
+    context RootKeyTestTwoSerializer do
       let(:serializable) { {} }
-      let(:serializer)   { TestTwoSerializer }
+      let(:serializer)   { RootKeyTestTwoSerializer }
 
       it 'has the correct root key' do
         expect(serialized).to include(:hashes)
